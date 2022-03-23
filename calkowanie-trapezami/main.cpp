d2e75019ef3574f5ff61cc5f79e2c40df6938a4b
Pomarancz789 <granat-02krawcy@icloud.com>
2022-03-23 10:38:20 +0100

    działa

--- calkowanie-trapezami/main.cpp
+++ calkowanie-trapezami/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  calkowanie-trapezami
-//
-//  Created by Jakub Wtorkiewicz on 23/03/2022.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+float f(float x)
+{
+    return x*x+1;
+}
+
+int main()
+{
+    float a=0;
+    float b=0;
+    int liczbaprost=0;
+    
+    
+    cout << "Podaj liczbe poczatkowa: ";
+    cin >> a;
+    cout << "Podaj liczbe koncowa: ";
+    cin >> b;
+    cout << "Podaj liczbe prostokatow: ";
+    cin >> liczbaprost;
+    
+    
+    float d=(b-a)/liczbaprost;
+    //f(x)=x^2 + 1
+    float wynik=0;
+    
+    for (float i=a; i<b; i+=d) {
+        wynik += (((f(i)+f(i+d))*d)/2);
+    }
+    
+    cout << "wynik to: " << wynik << endl;
+    
     return 0;
 }
