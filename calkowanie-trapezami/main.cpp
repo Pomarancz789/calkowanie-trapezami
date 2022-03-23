e5c7dfd0dad7e1bf2afb379d9fe74cab0cf12f06
Pomarancz789 <granat-02krawcy@icloud.com>
2022-03-23 10:28:08 +0100

    Initial Commit

---
+++ calkowanie-trapezami/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calkowanie-trapezami
+//
+//  Created by Jakub Wtorkiewicz on 23/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
